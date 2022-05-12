d136b6ff7e438f904024ed064c01b8b07069e90d
Alex Groat <alex.groat.95@gmail.com>
2022-05-12 16:52:41 +0930

    timestamp 20:00

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
     <!-- Search / Results -->
-    <div class="flex justify-center relative px-4 pt-8 pb-32"></div>
+    <div
+      class="
+        flex
+        justify-center
+        relative
+        bg-hero-pattern bg-cover
+        px-4
+        pt-8
+        pb-32
+      "
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            class="
+              flex-1
+              py-3
+              px-2
+              rounded-tl-md rounded-bl-md
+              focus:outline-none
+            "
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your IP info"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
